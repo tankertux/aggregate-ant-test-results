2afd61730fe52f40f804a859a3c6466f11c22654
Jordan Grant <JordanGrant@carfax.com>
2014-02-03 11:45:48 -0600

    Project to demonstrate aggregate reporting of Gradle Test and Ant Test results

---
+++ src/test/java/tankertux/DeleteThisTest.java
@@ -0,0 +1,13 @@
+package tankertux;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DeleteThisTest {
+
+    @Test
+    public void deleteThis(){
+        assertTrue(true);
+    }
+}
